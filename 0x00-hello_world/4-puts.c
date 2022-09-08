91e6511d5290331e20f70497e4be25c8e6f1da5c
Heroblack1 <danjumahero@gmail.com>
2022-09-08 15:39:24 -0700

    k

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 
 /**
  * main - print the string in the put function
- * this program prints "programming is  like building a multilingual puzzle
+ * this program prints "programming is  like building a multilingual puzzle"
  * Return: 0
  */
 int main(void)
