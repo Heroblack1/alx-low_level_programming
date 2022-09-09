3317ef633bb57bc474aba641245700aa38d3ded1
Heroblack1 <danjumahero@gmail.com>
2022-09-08 17:30:57 -0700

    g

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,7 +14,6 @@ int main(void)
 	long li;
         long long lli;
 	float f;
-
 	printf("size of a char: %ld byte(s)\n", sizeof(c));
 	printf("size of an int: %ld bytes(s)\n", sizeof(i));
 	printf("size of a long int: %ld byte(s)\n", sizeof(li));
