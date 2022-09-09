de5c1b6f5753b33c268b56edcb02119fd3839a96
Heroblack1 <danjumahero@gmail.com>
2022-09-08 17:01:01 -0700

    d

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
+ * main - print the string in the put function
  *
- *  * main - main block
- *
- *   * Return: 0
- *
- *    */
+ * Description - using the main function
+ * this program prints "programming is like building a multilingual puzzle
+ * Return: 0
+ */
 
 int main(void)
 
